ffbe248df836cb2aeca9c2f99c38ac1372201627
P1M5 <37663832+P1M5@users.noreply.github.com>
2024-11-12 22:10:13 +0200

    Init

---
+++ src/main/java/com/p1m5/alphastrength/AlphaStrengthApplication.java
@@ -0,0 +1,13 @@
+package com.p1m5.alphastrength;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaStrengthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaStrengthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/p1m5/alphastrength/AlphaStrengthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p1m5.alphastrength;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaStrengthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
